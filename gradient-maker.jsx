41e6e05e7a3d955b3b6468ab11e7cc5c1ca2645a
Micky Hulse <mickyhulse@gmail.com>
2016-05-14 02:46:55 -0700

    Pretty much finished

--- gradient-maker.jsx
+++ gradient-maker.jsx
@@ -1,5 +1,5 @@
-/* jshint laxbreak:true, -W043, -W030 */
-/* globals GradientType */
+/* jshint laxbreak:true, -W043 */
+/* globals GradientType, BridgeTalk */
 
 // jshint ignore:start
 #target illustrator
@@ -11,12 +11,108 @@
 // https://forums.adobe.com/message/8749234#8749234
 // https://addyosmani.com/resources/essentialjsdesignpatterns/book/#facadepatternjavascript
 
-var GRAD = (function(win, undefined) {
+var GRAD = (function(root, GradientType, BridgeTalk, Window, undefined) {
 	
+	// Private variable container object:
 	var _private = {};
 	
-	_private.doc = null;
+	// IMPORTANT! This MUST return a non-cached `activeDocument` result on each
+	// call due to the way Illustrator palettes communicate with the targe app.
+	_private.doc = (function() {
+		
+		return root.app.activeDocument;
+		
+	})();
 	
+	// Creates and returns a palette window object.
+	_private.palette = function(title) {
+		
+		// Palette box setup:
+		var meta = 'palette { \
+			orientation: "column", \
+			alignChildren: ["fill", "top"], \
+			margins: 10, \
+			group1: Group { \
+				alignChildren: ["fill", "top"], \
+				orientation: "row", \
+				make: Button { text: "Make Gradient" }, \
+				close: Button { text: "Close" } \
+			} \
+		}';
+		
+		// Instanciate `Window` class with setup from above:
+		var palette = new Window(meta, title, undefined, {
+			//option: value
+			//closeButton: false
+		});
+		
+		// Start button:
+		palette.group1.make.onClick = function() {
+			
+			// We HAVE to “reconnect” to the app using `BridgeTalk`:
+			_private.reconnect();
+			
+		};
+		
+		// Close and/or palette UI close buttons:
+		palette.group1.close.onClick = function() {
+			
+			palette.close();
+			
+		};
+		
+		palette.onClose = function() {
+			
+			// Do any cleanup or garbage collection?
+			
+		};
+		
+		return palette;
+		
+	};
+	
+	// Reconnect to the `activeDocument`:
+	_private.reconnect = function() {
+		
+		var bt;
+		
+		bt = new BridgeTalk();
+		
+		bt.target = 'illustrator';
+		
+		// Enter the script through the “back door”:
+		bt.body = 'GRAD.init({ backdoor: true })';
+		
+		bt.send();
+		
+	};
+	
+	// Check to see if gradient name already exists:
+	_private.exists = function(name) {
+		
+		var swatches = _private.doc.swatches;
+		var i;
+		var l;
+		var flag = false; // Assumed to not exist.
+		
+		for (i = 0, l = swatches.length; i < l; i++) {
+			
+			if (swatches[i].name == name) {
+				
+				// It already exists!
+				flag = true;
+				
+				break;
+				
+			}
+			
+		}
+		
+		return flag; // Single exit point.
+		
+	}
+	
+	// Creates a swatch title based on selected swatch names, or user input:
 	_private.title = function(swatches) {
 		
 		var user;
@@ -27,97 +123,154 @@ var GRAD = (function(win, undefined) {
 		
 		for (i = 0, l = swatches.length; i < l; i++) {
 			
+			// Add swatch name to array:
 			names[i] = swatches[i].name;
 			
 		}
 		
+		// Convert array to string with dashes inbetween:
 		name = names.join('-');
 		
+		// Max length of gradient swatch name is 31 characters:
+		name = name.split('').splice(0,28).join('') + '...'
+		
+		// Give user the option of naming the gradient swatch:
 		user = prompt('Enter name for new gradient swatch', name);
 		
+		// Return the user’s chosen name:
 		return user;
 		
 	};
 	
-	_private.main = function() {
+	// Makes the gradient swatch:
+	_private.make = function() {
 		
-		var swatches = _private.doc.swatches.getSelected();
+		var swatches = _private.doc.swatches.getSelected(); // Gets selected swatches.
 		var grad;
 		var i;
 		var l = swatches.length;
 		var stop;
+		var name;
 		
+		// Must have at least one swatch selected:
 		if (l) {
 			
-			// Making a new gradient by default must have 2 colors and a mid point.
-			// Once you have made a gradient and referenced it. You can use
-			// `gradientStops.add()`` to give you extra stops that you require.
-			//
-			// Gradient stops are ordered from left to right, no matter what order
-			// they were created in.
-			grad = _private.doc.gradients.add();
+			// Get desired gradient swatch name:
+			name = _private.title(swatches);
 			
-			grad.name = _private.title(swatches);
-			grad.type = GradientType.LINEAR;
-			
-			for (i = 0; i < l; i++) {
+			// Check if gradient swatch name already exists:
+			if ( ! _private.exists(name)) {
 				
-				if (i === 0) {
+				// Making a new gradient by default must have 2 colors and a mid point.
+				// Once you have made a gradient and referenced it. You can use
+				// `gradientStops.add()`` to give you extra stops that you require.
+				//
+				// Gradient stops are ordered from left to right, no matter what order
+				// they were created in.
+				grad = _private.doc.gradients.add();
+				
+				grad.name = name;
+				grad.type = GradientType.LINEAR;
+				
+				// Loop over selected swatches:
+				for (i = 0; i < l; i++) {
 					
-					grad.gradientStops[0].color = swatches[i].color;
-					grad.gradientStops[0].rampPoint = 0;
-					grad.gradientStops[0].midPoint = 50;
-					
-				} else if (i === (l - 1)) {
-					
-					grad.gradientStops[l - 1].color = swatches[i].color;
-					grad.gradientStops[l - 1].rampPoint = 100;
-					grad.gradientStops[l - 1].midPoint = 50;
-					
-				} else {
-					
-					stop = grad.gradientStops.add();
-					stop.color = swatches[i].color;
-					grad.gradientStops[i].rampPoint = ((i / (l - 1)) * 100);
-					grad.gradientStops[i].midPoint = 50;
+					if (i === 0) {
+						
+						// Colorize the first default gradient color:
+						grad.gradientStops[0].color = swatches[i].color;
+						grad.gradientStops[0].rampPoint = 0;
+						grad.gradientStops[0].midPoint = 50;
+						
+					} else if (i === (l - 1)) {
+						
+						// Colorize the last default gradient color:
+						grad.gradientStops[l - 1].color = swatches[i].color;
+						grad.gradientStops[l - 1].rampPoint = 100;
+						grad.gradientStops[l - 1].midPoint = 50;
+						
+					} else {
+						
+						// Fill-in the rest of the colors, evenly, between first
+						// and last color stops:
+						stop = grad.gradientStops.add();
+						stop.color = swatches[i].color;
+						grad.gradientStops[i].rampPoint = ((i / (l - 1)) * 100);
+						grad.gradientStops[i].midPoint = 50;
+						
+					}
 					
 				}
 				
+			} else {
+				
+				root.alert('A gradient swatch, by that name (' + name + '), already exists.');
+				
 			}
+			
+		} else {
+			
+			root.alert('You must select one or more swatches.');
 			
 		}
 		
 	};
 	
-	_private.init = function($title) {
+	// Script setup, if coming through the “front door”:
+	_private.init = function(title) {
 		
-		_private.doc = win.app.activeDocument;
+		var palette;
 		
 		// Open document(s)?
-		if (win.app.documents.length > 0) {
+		if (root.app.documents.length > 0) {
 			
-			// Begin the program:
-			_private.main(); // Only run if there's at least one document open.
+			_private.doc = root.app.activeDocument;
+			
+			// Make palette window:
+			palette = _private.palette();
+			
+			// Center and show the palette window:
+			palette.center();
+			palette.show();
 			
 		} else {
 			
 			// Nope, let the user know what they did wrong:
-			win.alert('You must open at least one document.');
+			root.alert('You must open at least one document.');
 			
 		}
 		
 	};
 	
+	// Public API:
 	return {
 		init: function(args) {
 			
-			_private.init();
+			// Entry point for `BridgeTalk` and script testing:
+			if (
+				args.backdoor
+				&&
+				(args.backdoor.toString() == 'true')
+			) {
+				
+				// Jump straight to the `prompt`:
+				_private.make();
+				
+			} else {
+				
+				// Fire-up the palette window:
+				_private.init(args.title);
+				
+			}
 			
 		}
 	};
 	
-})(this);
+})(this, GradientType, BridgeTalk, Window);
 
 GRAD.init({
+	//backdoor: true, // Use this for testing!
 	title: 'Gradient Maker'
 });
+
+// Done!
---
+++ install.sh
@@ -0,0 +1,108 @@
+#!/bin/bash
+
+# http://www.unixcl.com/2008/03/creating-menus-using-select-bash.html
+# http://askubuntu.com/questions/1705/how-can-i-create-a-select-menu-in-a-shell-script
+# http://tldp.org/LDP/Bash-Beginners-Guide/html/sect_09_06.html
+# http://unix.stackexchange.com/questions/257290
+# http://www.thegeekstuff.com/2010/07/bash-case-statement/
+
+# Install files:
+function install() {
+	
+	# Installation folder:
+	target="@mhulse"
+	
+	# Illustrator default folder:
+	INSTALL_DIR="/Applications/$1/Presets.localized/en_US/Scripts/$target"
+	
+	# Create installation folder:
+	mkdir -pv "$INSTALL_DIR"
+	
+	# Remote repo zip file:
+	SOURCE_ZIP="https://github.com/mhulse/illy-grad/tarball/master"
+	
+	# Get the zip file and extract all files:
+	curl -sS -#L "$SOURCE_ZIP" | tar -xzv --strip-components 1 -C "$INSTALL_DIR" "*.jsx"
+	
+	# Let the use know that we are done:
+	echo $'\n'"Congrats! Installation was successful!"$'\n'
+	
+	# Open installation folder:
+	open "$INSTALL_DIR"
+	
+}
+
+# Check if Illustrator is installed:
+function check() {
+	
+	for f in "$1"; do
+		
+		# Check if the glob gets expanded to existing files.
+		# If not, f here will be exactly the pattern above
+		# and the exists test will evaluate to false.
+		[ -e "$f" ] && return 0  || return 1 # 0 = true, 1 = false
+		
+	done
+	
+}
+
+# Pick Illustrator version:
+function choose() {
+	
+	# Set the prompt string used by `select`:
+	PS3=$'\n'"Enter a number: "
+	
+	echo "Please pick the version of Illustrator"
+	echo "you would like to install these files to:"$'\n'
+	
+	select folder in "Cancel" "$1"
+		do
+			case $folder in
+				"Cancel")
+					# User cancled:
+					echo "Exiting …"
+					return 1
+					;;
+				*[![:blank:]]*)
+					# Not blank or empty and is set:
+					echo $'\n'"Installing files into $folder …"$'\n'
+					break
+					;;
+				*)
+					# Contains only blanks, is empty or unset:
+					echo $'\n'"Your choice (${REPLY}) does not exist!"
+					echo "Please try again or enter 1 to cancel."
+					;;
+			esac
+		done
+	
+}
+
+# Tidy up the terminal window:
+clear
+
+# Switch to glob folder location:
+cd "/Applications"
+
+if check "Adobe Illustrator"*; then
+	
+	# Create menu:
+	if choose "Adobe Illustrator"*; then # `$folder` is now a global.
+		
+		install "$folder"
+		
+	fi
+	
+else
+	
+	echo "Sorry, but Illustrator is not installed."
+	echo "Please install Illustrator and try again."$'\n'
+	
+fi
+
+# Return the user to where they started:
+cd - > /dev/null
+
+# Done!
+# For more information about this script, see:
+# https://github.com/mhulse/illy-grad
